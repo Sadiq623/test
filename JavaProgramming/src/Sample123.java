9776cfef8666d5d5b1c41180543b22c1f3fea568
sadiq <sadiqhussain623@gmail.com>
2020-01-01 12:53:29 +0530

    Sadiq:modified file

--- JavaProgramming/src/Sample123.java
+++ JavaProgramming/src/Sample123.java
@@ -4,6 +4,8 @@ public class Sample123 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Happy new year 2020");
+		
+		System.out.println("Welcome to infosys");
 	}
 
 }
